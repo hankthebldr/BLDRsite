9f541e1461a76349192b1444e43ffb01975237b9
hankthebldr <henry.reed@icloud.com>
2022-03-10 21:59:07 -0500

    node package updates

--- BLDRweb/src/js/user.js
+++ BLDRweb/src/js/user.js
@@ -1,4 +1,25 @@
-// 
 // user.js
 // Use this to write your custom JS
-//
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDbimyUq8bz8SaRx7drqkmNRUmAI2tK-a8",
+  authDomain: "bldrstock.firebaseapp.com",
+  databaseURL: "https://bldrstock-default-rtdb.firebaseio.com",
+  projectId: "bldrstock",
+  storageBucket: "bldrstock.appspot.com",
+  messagingSenderId: "775646920154",
+  appId: "1:775646920154:web:a9e792bb5e037f952094e3",
+  measurementId: "G-FMFCB0BNRH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
